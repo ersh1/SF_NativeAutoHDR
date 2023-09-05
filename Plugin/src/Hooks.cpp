e050a3b3700fe74920f1a46b610661c8eff43d85
ersh1 <16157893+ersh1@users.noreply.github.com>
2023-09-05 11:18:42 +0200

    Version 2.0.0 [asi] - ImageSpaceBuffer is now upgraded as well

--- Plugin/src/Hooks.cpp
+++ Plugin/src/Hooks.cpp
@@ -12,9 +12,39 @@ namespace Hooks
 		_UnkFunc(a1, a2);
     }
 
-	void Install()
+#ifndef NDEBUG
+    DXGI_FORMAT DebugHooks::Hook_GetFormat(uint32_t a_format)
+    {
+		auto format = _GetFormat(a_format);
+		if (format == DXGI_FORMAT_R8G8B8A8_UNORM || format == DXGI_FORMAT_R8G8B8A8_UNORM_SRGB) {
+		    return DXGI_FORMAT_R16G16B16A16_FLOAT;
+		}
+
+		return format;
+    }
+
+    void DebugHooks::Hook_CreateRenderTargetView(uintptr_t a1, ID3D12Resource* a_resource, DXGI_FORMAT a_format, uint8_t a4, uint16_t a5, uintptr_t a6)
+    {
+		const auto textureDesc = a_resource->GetDesc();
+
+		_CreateRenderTargetView(a1, a_resource, a_format, a4, a5, a6);
+    }
+
+    void DebugHooks::Hook_CreateDepthStencilView(uintptr_t a1, ID3D12Resource* a_resource, DXGI_FORMAT a_format, uint8_t a4, uint16_t a5, uintptr_t a6)
+    {
+		const auto textureDesc = a_resource->GetDesc();
+
+		_CreateDepthStencilView(a1, a_resource, a_format, a4, a5, a6);
+    }
+#endif
+
+    void Install()
 	{
 		Hooks::Hook();
 		Patches::Patch();
+
+#ifndef NDEBUG
+		DebugHooks::Hook();
+#endif
 	}
 }
--- Plugin/src/Hooks.h
+++ Plugin/src/Hooks.h
@@ -2,6 +2,7 @@
 #include "Settings.h"
 
 #include <dxgi1_6.h>
+#include <d3d12.h>
 
 namespace Hooks
 {
@@ -10,15 +11,31 @@ namespace Hooks
     public:
 		static void Patch()
 		{
-			const auto scan = static_cast<uint8_t*>(dku::Hook::Assembly::search_pattern<"C6 45 68 01 8B 05 ?? ?? ?? ??">());
-			if (!scan) {
-			    ERROR("Failed to find format variable");
-			}
-			const auto offset = *reinterpret_cast<int32_t*>(scan + 6);
-			uint32_t* format = reinterpret_cast<uint32_t*>(scan + 10 + offset);  // 507A290
+			uint32_t* frameBufferPtr = nullptr;
+			uint32_t* imageSpaceBufferPtr = nullptr;
 
+			{
+				const auto scan = static_cast<uint8_t*>(dku::Hook::Assembly::search_pattern<"C6 45 68 01 8B 05 ?? ?? ?? ??">());
+				if (!scan) {
+					ERROR("Failed to find FrameBuffer format variable")
+				}
+				const auto offset = *reinterpret_cast<int32_t*>(scan + 6);
+				frameBufferPtr = reinterpret_cast<uint32_t*>(scan + 10 + offset);  // 507A290
+			}
+			
+			{
+				const auto scan = static_cast<uint8_t*>(dku::Hook::Assembly::search_pattern<"44 8B 05 ?? ?? ?? ?? 89 55 FB">());
+				if (!scan) {
+					ERROR("Failed to find ImageSpaceBuffer format variable")
+				}
+				const auto offset = *reinterpret_cast<int32_t*>(scan + 3);
+				imageSpaceBufferPtr = reinterpret_cast<uint32_t*>(scan + 7 + offset);  // 5079A70
+			}
+			
 			const auto settings = Settings::Main::GetSingleton();
-			*format = settings->scRGB ? 77 : 62;
+
+			*frameBufferPtr = settings->scRGB ? 77 : 62;
+			*imageSpaceBufferPtr = settings->scRGB ? 77 : 62;
 		}
 
     private:
@@ -46,7 +63,7 @@ namespace Hooks
 		{
 			const auto scan = static_cast<uint8_t*>(dku::Hook::Assembly::search_pattern<"E8 ?? ?? ?? ?? 8B 4D A8 8B 45 AC">());
 			if (!scan) {
-				ERROR("Failed to find callsite");
+				ERROR("Failed to find callsite")
 			}
 			const auto callsiteOffset = *reinterpret_cast<int32_t*>(scan + 1);
 			const auto UnkFuncCallsite = AsAddress(scan + 5 + callsiteOffset + 0x3EA);
@@ -59,6 +76,34 @@ namespace Hooks
 
 		static inline std::add_pointer_t<decltype(Hook_UnkFunc)> _UnkFunc;
 	};
+
+#ifndef NDEBUG
+	class DebugHooks
+	{
+	public:
+		static void Hook()
+		{
+			const auto callsite1 = AsAddress(dku::Hook::Module::get().base() + 0x32ED294);
+			const auto callsite2 = AsAddress(dku::Hook::Module::get().base() + 0x32ED341);
+			const auto callsite3 = AsAddress(dku::Hook::Module::get().base() + 0x32ED2C1);
+			const auto callsite4 = AsAddress(dku::Hook::Module::get().base() + 0x32ED37F);
+
+			_CreateRenderTargetView = dku::Hook::write_call<5>(callsite1, Hook_CreateRenderTargetView);
+			dku::Hook::write_call<5>(callsite2, Hook_CreateRenderTargetView);
+			_CreateDepthStencilView = dku::Hook::write_call<5>(callsite3, Hook_CreateDepthStencilView);
+			dku::Hook::write_call<5>(callsite4, Hook_CreateDepthStencilView);
+		}
+
+	private:
+		static DXGI_FORMAT Hook_GetFormat(uint32_t a_format);
+		static void Hook_CreateRenderTargetView(uintptr_t a1, ID3D12Resource* a_resource, DXGI_FORMAT a_format, uint8_t a4, uint16_t a5, uintptr_t a6);
+		static void Hook_CreateDepthStencilView(uintptr_t a1, ID3D12Resource* a_resource, DXGI_FORMAT a_format, uint8_t a4, uint16_t a5, uintptr_t a6);
+
+		static inline std::add_pointer_t < decltype(Hook_GetFormat)> _GetFormat;
+		static inline std::add_pointer_t<decltype(Hook_CreateRenderTargetView)> _CreateRenderTargetView;
+		static inline std::add_pointer_t<decltype(Hook_CreateDepthStencilView)> _CreateDepthStencilView;
+	};
+#endif
 
     void Install();
 }
